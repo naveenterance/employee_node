8c303ce7b7a9f8007bc62a9a9a8063555256a9d3
Naveen <naveenterance@gmail.com>
2021-11-02 16:37:39 +0530

    First

---
+++ models/db.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb+srv://yoda:force@cluster0.mlnry.mongodb.net/EmployeeDB?retryWrites=true&w=majority', { useNewUrlParser: true }, (err) => {
+    if (!err) { console.log('MongoDB Connection Succeeded.') }
+    else { console.log('Error in DB connection : ' + err) }
+});
+
+require('./employee.model');
